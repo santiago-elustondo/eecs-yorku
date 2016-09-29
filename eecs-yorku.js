bc9cb0b2079da11c9faff3eee991c625de32f0ba
santi <elus2012@gmail.com>
2016-09-29 14:40:45 -0400

    fix: APP_DATA check. and check node version

--- eecs-yorku.js
+++ eecs-yorku.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 
+var compareVersions = require('compare-versions')
 var path = require('path')
+var fs = require('fs')
+var fsPromise = require('fs-es6-promise')
 var OS = require('os')
 var SshClient = require('node-ssh')
 var JsonFile = require('jsonfile') 
 
+const NODE_MIN_VERSION = '6.7.0'
 const HOST = 'red.cse.yorku.ca'
 const HOME_PATH = '/eecs/home'
-const APP_DATA = path.resolve(OS.homedir(), 'eecs-yorku')
+const LOCAL_HOME = OS.homedir()
+const APP_DATA = path.resolve(LOCAL_HOME, 'eecs-yorku')
 const USER_FILE = path.resolve(APP_DATA, 'credentials.json')
 const msg = {
   CONFIGURE_CREDENTIALS: 'You must configure your credentials.',
@@ -16,11 +21,16 @@ const msg = {
   PROVIDE_USER: 'Provide user as second parameter',
   PROVIDE_PASS: 'Provide password as second parameter',
   INVALID_ACTION: 'Invalid action. try: submit, set-user, set-pass',
-  COULDNT_WRITE: 'Attempt to write to file failed. Did you try "sudo"?'
+  COULDNT_WRITE: 'Attempt to write to file failed. Did you try "sudo"?',
+  HOME_DIR_PERMISSION: 'Process needs permission to write to home dir',
+  APP_DATA_PERMISSION: 'Process cannot write to <home-dir>/eecs-yorku',
+  NODE_VERSION: 'Please install Node version 6.7.0 or higher.'
 }
 
+(checkNodeVersion() || process.exit());
+
 var client = new SshClient()
-var close = (msg) => { console.log(msg || 'closing..'); client.dispose(); }
+var close = (msg) => { console.log(msg || 'closing..'); client.dispose(); process.exit(); }
 var error = (t, m, r) => { 
   var o = Object.create({type:t, message:m}); if(r){ r(o) } else { throw o } 
 }
@@ -28,25 +38,28 @@ var error = (t, m, r) => {
 var args = getArgs();
 var user;
 
-switch(args.action){
-  case 'submit':
-    getUser()
-      .then(connect)
-      .then(() => uploadSubmissions(client, args.file_names))
-      .then(dir => submitFiles(dir, args.file_names))
-      .then(res => close(res))
-      .catch(e => close(e.message)); break;
-  case 'set-user':
-    setUser(args.username)
-      .then(() => console.log('user set'))
-      .catch(e => close(e.message)); break;
-  case 'set-pass':
-    setPassword(args.password)
-      .then(() => console.log('password set'))  
-      .catch(e => close(e.message)); break;
-  default: 
-    console.log(msg.INVALID_ACTION); break;
-}
+checkDataStorage()
+.then(() =>{
+  switch(args.action){
+    case 'submit':
+      getUser()
+        .then(connect)
+        .then(() => uploadSubmissions(client, args.file_names))
+        .then(dir => submitFiles(dir, args.file_names))
+        .then(res => close(res))
+        .catch(e => close(e.message)); break;
+    case 'set-user':
+      setUser(args.username)
+        .then(() => console.log('user set'))
+        .catch(e => close(e.message)); break;
+    case 'set-pass':
+      setPassword(args.password)
+        .then(() => console.log('password set'))  
+        .catch(e => close(e.message)); break;
+    default: 
+      console.log(msg.INVALID_ACTION); break;
+  }
+}).catch(e => close(e.message))
 
 
 //-----------------------
@@ -156,4 +169,29 @@ function uploadFile(client, fname, dir){
         error('NOT_FOUND', fname + ' not found')
       else error('UNKNOWN', 'unknown error: could not upload file')
     })
+}
+
+
+function checkDataStorage(){
+  return fsPromise.access(LOCAL_HOME, fs.constants.W_OK)
+  .catch(err => error('NO_PERMISSION', msg.HOME_DIR_PERMISSION))
+  .then(() => fsPromise
+    .access(APP_DATA, fs.constants.F_OK)
+    .catch(() => fsPromise.mkdir(APP_DATA) // try to make APP_DATA folder
+      .catch(err => error('NO_PERMISSION', msg.APP_DATA_PERMISSION))
+    )
+  )
+}
+
+
+function checkNodeVersion(){
+  var v = process.versions.node;
+  var c = compareVersions(v, NODE_MIN_VERSION);
+  var r = true;
+  if(c < 0) { 
+    console.log(msg.NODE_VERSION);
+    console.log('Current Version: ' + v);
+    r = false;
+  }
+  return r;
 }
